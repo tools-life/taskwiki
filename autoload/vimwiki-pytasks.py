2046ddc81c882ef8fa8949868d744fb933547f19
Tomas Babej <tomasbabej@gmail.com>
2015-01-01 15:41:02 +0100

    Regexps: Refactor TEXT regexp

--- autoload/vimwiki-pytasks.py
+++ autoload/vimwiki-pytasks.py
@@ -5,20 +5,28 @@ from tasklib.task import TaskWarrior, Task
 
 # Unnamed building blocks
 UUID_UNNAMED = r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
+DUE_UNNAMED = r'\(\d{4}-\d\d-\d\d( \d\d:\d\d)?\)'
 SPACE_UNNAMED = r'\s*'
 NONEMPTY_SPACE_UNNAMED = r'\s+'
 FINAL_SEGMENT_SEPARATOR_UNNAMED = r'(\s+|$)'
+
+TEXT_FORBIDDEN_SUFFIXES = (
+    r'\s',  # Text cannot end with whitespace
+    r' !', r' !!', r' !!!',  # Any priority value
+    DUE_UNNAMED,
+    UUID_UNNAMED,  # Text cannot end with UUID
+)
 
 # Building blocks
 BRACKET_OPENING = re.escape('* [')
 BRACKET_CLOSING = re.escape('] ')
 EMPTY_SPACE = r'(?P<space>\s*)'
 UUID = r'(?P<uuid>{0})'.format(UUID_UNNAMED)
-TEXT = r'(?P<text>.+(?<!{0})(?<!\s))'.format(UUID_UNNAMED)
-DUE = r'(?P<due>\(\d{4}-\d\d-\d\d( \d\d:\d\d)?\))'
+DUE = r'(?P<due>{0})'.format(DUE_UNNAMED)
+UUID_COMMENT = '#{0}'.format(UUID)
+TEXT = r'(?P<text>.+' + ''.join(['(?<!%s)' % suffix for suffix in TEXT_FORBIDDEN_SUFFIXES])
 COMPLETION_MARK = r'(?P<completed>.)'
 PRIORITY = r'(?P<priority>!{1,3})'
-UUID_COMMENT = '#{0}'.format(UUID)
 
 # Middle building blocks
 INCOMPLETE_TASK_PREFIX = EMPTY_SPACE + BRACKET_OPENING + '[^X]' + BRACKET_CLOSING + TEXT
