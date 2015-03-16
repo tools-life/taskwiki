6b4b6c7e0951e2fd5513f385f79a9089e80b1125
Tomas Babej <tomasbabej@gmail.com>
2015-03-16 06:25:25 +0100

    VimwikiTask: Store data in a data dict

--- taskwiki/task.py
+++ taskwiki/task.py
@@ -21,6 +21,13 @@ class VimwikiTask(object):
         self.cache = cache
         self.tw = cache.tw
         self._task = None
+        self.data = dict()
+
+    def __getitem__(self, key):
+        return self.data.get(key)
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
 
     @classmethod
     def from_line(cls, cache, number):
@@ -36,31 +43,33 @@ class VimwikiTask(object):
 
         self = cls(cache)
 
-        self.indent = match.group('space')
-        self.text = match.group('text')
-        self.uuid = match.group('uuid')  # can be None for new tasks
-        self.due = match.group('due')  # TODO: convert to proper timestamp
-        self.completed_mark = match.group('completed')
-        self.completed = self.completed_mark is 'X'
-        self.line_number = number
-        self.priority = len(match.group('priority') or []) # This is either 0,1,2 or 3
+        self.data.update({
+            'indent': match.group('space'),
+            'description': match.group('text'),
+            'uuid': match.group('uuid'),  # can be None for new tasks
+            'completed_mark': match.group('completed'),
+            'completed': match.group('completed') is 'X',
+            'line_number': number,
+            'priority': convert_priority_to_tw_format(
+                len(match.group('priority') or [])) # This is either 0,1,2 or 3
 
         # To get local time aware timestamp, we need to convert to from local datetime
         # to UTC time, since that is what tasklib (correctly) uses
+        due = match.group('due')
         if self.due:
             # With strptime, we get a native datetime object
             try:
-                parsed_due = datetime.strptime(self.due, DATETIME_FORMAT)
+                parsed_due = datetime.strptime(due, DATETIME_FORMAT)
             except ValueError:
                 try:
-                    parsed_due = datetime.strptime(self.due, DATE_FORMAT)
+                    parsed_due = datetime.strptime(due, DATE_FORMAT)
                 except ValueError:
                     vim.command('echom "Taskwiki: Invalid timestamp on line %s, '
                                 'ignored."' % self.line_number)
 
             # We need to interpret it as timezone aware object in user's timezone
             # This properly handles DST, timezone offset and everything
-            self.due = SerializingObject().datetime_normalizer(parsed_due)
+            self['due'] = SerializingObject().datetime_normalizer(parsed_due)
 
         # We need to track depedency set in a extra attribute, since
         # this may be a new task, and hence it need not to be saved yet.
@@ -73,7 +82,7 @@ class VimwikiTask(object):
         if self.parent:
             self.parent.add_dependencies |= set([self])
 
-        self.project = self.find_project()
+        self['project'] = self.find_project()
 
     @classmethod
     def from_task(cls, cache, task):
@@ -88,15 +97,15 @@ class VimwikiTask(object):
             return self._task
 
         # Else try to load it or create a new one
-        if self.uuid:
+        if self['uuid']:
             try:
-                self._task = self.cache[self.uuid]
+                self._task = self.cache[self['uuid']]
             except Task.DoesNotExist:
                 self._task = Task(self.tw)
                 # If task cannot be loaded, we need to remove the UUID
                 vim.command('echom "UUID not found: %s,'
                             'will be replaced if saved"' % self.uuid)
-                self.uuid = None
+                self['uuid'] = None
         else:
             self._task = Task(self.tw)
 
@@ -105,10 +114,12 @@ class VimwikiTask(object):
     @task.setter
     def task(self, task):
         # Make sure we're updating by a correct task
-        if task['uuid'] != self.uuid:
+        if task['uuid'] != self['uuid']:
             raise ValueError("Task '%s' with '%s' cannot be updated by "
                              "task with uuid '%s'."
-                             % (self.text, self.uuid, task['uuid']))
+                             % (self['description'],
+                                self['uuid'],
+                                task['uuid']))
 
         self._task = task
 
@@ -118,15 +129,15 @@ class VimwikiTask(object):
 
     @property
     def priority_to_tw_format(self):
-        return convert_priority_to_tw_format(self.priority)
+        return convert_priority_to_tw_format(self['priority'])
 
     @property
     def tainted(self):
         return any([
-            self.task['description'] != self.text,
-            self.task['priority'] != self.priority_to_tw_format,
-            self.task['due'] != self.due,
-            self.task['project'] != self.project,
+            self.task['description'] != self['description'],
+            self.task['priority'] != self['priority'],
+            self.task['due'] != self['due'],
+            self.task['project'] != self['project'],
         ])
 
     def save_to_tw(self):
@@ -135,27 +146,27 @@ class VimwikiTask(object):
         # somehow differs
         # TODO: Check more than description
         if self.tainted:
-            self.task['description'] = self.text
-            self.task['priority'] = self.priority_to_tw_format
-            self.task['due'] = self.due
+            self.task['description'] = self['description']
+            self.task['priority'] = self['priority']
+            self.task['due'] = self['due']
             # TODO: this does not solve the issue of changed or removed deps (moved task)
             self.task['depends'] |= set(s.task for s in self.add_dependencies
                                         if not s.task.completed)
             # Since project is not updated in vimwiki on change per task, push to TW only
             # if defined
             if self.project:
-                self.task['project'] = self.project
+                self.task['project'] = self['project']
             self.task.save()
 
             # If we saved the task, we need to update. Hooks may have chaned data.
-            self.update_from_task(refresh=False)
+            self.update_from_task()
 
         # Load the UUID
-        if not self.uuid:
-            self.uuid = self.task['uuid']
+        if not self['uuid']:
+            self['uuid'] = self.task['uuid']
 
         # Mark task as done. This works fine with already completed tasks.
-        if self.completed and (self.task.pending or self.task.waiting):
+        if self['completed'] and (self.task.pending or self.task.waiting):
             self.task.done()
 
     def update_from_task(self):
@@ -173,21 +184,21 @@ class VimwikiTask(object):
 
     def __str__(self):
         return ''.join([
-            self.indent,
+            self['indent'],
             '* [',
-            'X' if self.completed else self.completed_mark,
+            'X' if self['completed'] else self['completed_mark'],
             '] ',
-            self.text if self.text else 'TEXT MISSING?',
-            ' ' + '!' * self.priority if self.priority else '',
-            ' ' + self.due.strftime(DATETIME_FORMAT) if self.due else '',
-            '  #' + self.uuid if self.uuid else '',
+            self.text if self['description'] else 'TEXT MISSING?',
+            ' ' + '!' * self.priority_from_tw_format if self['priority'] else '',
+            ' ' + self['due'].strftime(DATETIME_FORMAT) if self['due'] else '',
+            '  #' + self['uuid'] if self['uuid'] else '',
         ])
 
     def find_parent_task(self):
         for i in reversed(range(0, self.line_number)):
             if re.search(GENERIC_TASK, vim.current.buffer[i]):
                 task = self.cache[i]
-                if len(task.indent) < len(self.indent):
+                if len(task['indent']) < len(self['indent']):
                     return task
 
     def find_project(self):
