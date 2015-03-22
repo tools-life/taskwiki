a547c30c71a79a532d55a7b33b14ae3e42fb413d
Tomas Babej <tomasbabej@gmail.com>
2015-03-22 11:53:35 +0100

    util: Add ability to replace splits of the same kind

--- taskwiki/util.py
+++ taskwiki/util.py
@@ -2,6 +2,7 @@
 import vim  # pylint: disable=F0401
 import regexp
 import random
+import os
 
 # Detect if command AnsiEsc is available
 ANSI_ESC_AVAILABLE = vim.eval('exists(":AnsiEsc")') == '2'
@@ -93,6 +94,12 @@ def get_current_line_number():
     row, column = vim.current.window.cursor
     return row - 1
 
+def get_valid_tabpage_buffers(tabpage):
+    return [win.buffer for win in tabpage.windows if win.buffer.valid]
+
+def buffer_shortname(buffer):
+    return os.path.basename(buffer.name)
+
 def selected_line_numbers():
     return range(vim.current.range.start, vim.current.range.end + 1)
 
@@ -100,9 +107,20 @@ def strip_ansi_escape_sequence(string):
     return regexp.ANSI_ESCAPE_SEQ.sub("", string)
 
 def show_in_split(lines, size=None, position="belowright", vertical=False,
-                  name="taskwiki"):
+                  name="taskwiki", replace_opened=True):
+
+    # If the multiple buffers with this name are not desired
+    # cloase all the old ones in this tabpage
+    if replace_opened:
+        for buf in get_valid_tabpage_buffers(vim.current.tabpage):
+            shortname = buffer_shortname(buf)
+            if shortname.startswith(name):
+                vim.command('bwipe {0}'.format(shortname))
 
     # Generate a random suffix for the buffer name
+    # This is needed since AnsiEsc saves the buffer name inside
+    # s: scoped variables. Also lowers the probability of clash with
+    # a real file.
     random_suffix = random.randint(1,100000)
     name = '{0}.{1}'.format(name, random_suffix)
 
