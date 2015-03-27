85b4c2a3966c09bdfebc788b7e3d31fbb8285b77
Tomas Babej <tomasbabej@gmail.com>
2015-03-27 21:07:14 +0100

    tests: Add tests for TaskWikiDelete

--- tests/test_selected.py
+++ tests/test_selected.py
@@ -80,3 +80,94 @@ class TestAnnotateActionRange(IntegrationTest):
         annotation = self.tasks[1]['annotations']
         assert annotation != []
         assert annotation[0]['description'] == "This is annotation."
+
+
+class TestDeleteAction(IntegrationTest):
+
+    viminput = """
+    * [ ] test task 1  #{uuid}
+    * [ ] test task 2  #{uuid}
+    """
+
+    vimoutput = """
+    * [ ] test task 2  #{uuid}
+    """
+
+    tasks = [
+        dict(description="test task 1"),
+        dict(description="test task 2"),
+    ]
+
+    def execute(self):
+        self.command(
+            "TaskWikiDelete",
+            regex="Task \"test task 1\" deleted.$",
+            lines=1)
+
+        for task in self.tasks:
+            task.refresh()
+
+        assert self.tasks[0]['status'] == "deleted"
+        assert self.tasks[1]['status'] == "pending"
+
+
+class TestDeleteActionMoved(IntegrationTest):
+
+    viminput = """
+    * [ ] test task 1  #{uuid}
+    * [ ] test task 2  #{uuid}
+    """
+
+    vimoutput = """
+    * [ ] test task 1  #{uuid}
+    """
+
+    tasks = [
+        dict(description="test task 1"),
+        dict(description="test task 2"),
+    ]
+
+    def execute(self):
+        self.client.type('2gg')
+        self.command(
+            "TaskWikiDelete",
+            regex="Task \"test task 2\" deleted.$",
+            lines=1)
+        sleep(1)
+
+        for task in self.tasks:
+            task.refresh()
+
+        assert self.tasks[1]['status'] == "deleted"
+        assert self.tasks[0]['status'] == "pending"
+
+
+class TestDeleteActionRange(IntegrationTest):
+
+    viminput = """
+    * [ ] test task 1  #{uuid}
+    * [ ] test task 2  #{uuid}
+    """
+
+    vimoutput = """
+    """
+
+    tasks = [
+        dict(description="test task 1"),
+        dict(description="test task 2"),
+    ]
+
+    def execute(self):
+        self.client.normal('1gg')
+        sleep(1)
+        self.client.normal('VG')
+        sleep(1)
+        self.client.feedkeys(":TaskWikiDelete")
+        self.client.type('<Enter>')
+        sleep(1)
+
+        for task in self.tasks:
+            task.refresh()
+
+        assert self.tasks[1]['status'] == "deleted"
+        assert self.tasks[0]['status'] == "deleted"
