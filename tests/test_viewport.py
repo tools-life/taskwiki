03ebab5464334e4da6558fecdea4f5dc67e8e651
Tomas Babej <tomasbabej@gmail.com>
2015-03-28 03:03:18 +0100

    tests: Add functional tests for viewports overriding and removing values in default section

--- tests/test_viewport.py
+++ tests/test_viewport.py
@@ -60,3 +60,46 @@ class TestViewportDefaultsAssigment(IntegrationTest):
         assert task['status'] == 'pending'
         assert task['tags'] == ['work']
 
+
+class TestViewportDefaultsOverriding(IntegrationTest):
+
+    viminput = """
+    === Work tasks | project:Home +home | project:Chores  ===
+    * [ ] home task
+    """
+
+    vimoutput = """
+    === Work tasks | project:Home +home | project:Chores  ===
+    """
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+        assert len(self.tw.tasks.pending()) == 1
+
+        task = self.tw.tasks.pending()[0]
+        assert task['description'] == 'home task'
+        assert task['status'] == 'pending'
+        assert task['project'] == 'Chores'
+        assert task['tags'] == ['home']
+
+
+class TestViewportDefaultsRemoval(IntegrationTest):
+
+    viminput = """
+    === Work tasks | project:Home +home | project:  ===
+    * [ ] home task
+    """
+
+    vimoutput = """
+    === Work tasks | project:Home +home | project:  ===
+    """
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+        assert len(self.tw.tasks.pending()) == 1
+
+        task = self.tw.tasks.pending()[0]
+        assert task['description'] == 'home task'
+        assert task['status'] == 'pending'
+        assert task['project'] == None
+        assert task['tags'] == ['home']
