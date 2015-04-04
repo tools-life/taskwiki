98d8716192bfb6b4223d84855f647e2b698b5f19
Tomas Babej <tomasbabej@gmail.com>
2015-04-04 11:12:07 +0200

    tests: Add test for viewport inspection

--- tests/test_viewport.py
+++ tests/test_viewport.py
@@ -1,4 +1,5 @@
 from tests.base import IntegrationTest
+from time import sleep
 
 
 class TestViewportsTaskGeneration(IntegrationTest):
@@ -103,3 +104,35 @@ class TestViewportDefaultsRemoval(IntegrationTest):
         assert task['status'] == 'pending'
         assert task['project'] == None
         assert task['tags'] == ['home']
+
+
+class TestViewportInspection(IntegrationTest):
+
+    viminput = """
+    === Work tasks | +work ===
+    * [ ] tag work task  #{uuid}
+    """
+
+    vimoutput = """
+    ViewPort inspection:
+    --------------------
+    Name: Work tasks
+    Filter used: -DELETED +work
+    Defaults used: tags:['work']
+    Matching taskwarrior tasks: 1
+    Displayed tasks: 1
+    Tasks to be added:
+    Tasks to be deleted:
+    """
+
+    tasks = [
+        dict(description="tag work task", tags=['work']),
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+        self.client.feedkeys('1gg')
+        self.client.feedkeys(r'\<CR>')
+        sleep(0.5)
+
+        assert self.command(":py print vim.current.buffer", regex="<buffer taskwiki.")
