1e058ac349a2ba6cf093873bd5c542f3a9802afe
Tomas Babej <tomasbabej@gmail.com>
2015-12-25 21:15:01 +0100

    cache: Move Store objects into a separate module

--- taskwiki/cache.py
+++ taskwiki/cache.py
@@ -1,119 +1,10 @@
 import vim  # pylint: disable=F0401
 import re
 
-import util
 import vwtask
 import viewport
 import regexp
-
-from tasklib import TaskWarrior
-
-
-class WarriorStore(object):
-    """
-    Stores all instances of TaskWarrior objects.
-    """
-
-    def __init__(self):
-        # Determine defaults
-        default_rc = vim.vars.get('taskwiki_taskrc_location') or '~/.taskrc'
-        default_data = vim.vars.get('taskwiki_data_location') or '~/.task'
-
-        default_kwargs = dict(
-            data_location=default_data,
-            taskrc_location=default_rc,
-        )
-
-        # Setup the store of TaskWarrior objects
-        self.warriors = {'default': TaskWarrior(**default_kwargs)}
-        extra_warrior_defs = vim.vars.get('taskwiki_extra_warriors', {})
-
-        for key in extra_warrior_defs.keys():
-            current_kwargs = default_kwargs.copy()
-            current_kwargs.update(extra_warrior_defs[key])
-            self.warriors[key] = TaskWarrior(**current_kwargs)
-
-        # Make sure context is not respected in any TaskWarrior
-        for tw in self.warriors.values():
-            tw.overrides.update({'context':''})
-
-    def __getitem__(self, key):
-        try:
-            return self.warriors[key]
-        except KeyError as e:
-            raise errors.TaskWikiException(
-                "Taskwarrior with key '{0}' not available."
-                .format(key))
-
-
-    def __setitem__(self, key, value):
-        self.warriors[key] = value
-
-    def values(self):
-        return self.warriors.values()
-
-    def iteritems(self):
-        return self.warriors.iteritems()
-
-
-class NoNoneStore(object):
-
-    def __init__(self, cache):
-        self.cache = cache
-        self.store = dict()
-
-    def __getitem__(self, key):
-        item = self.store.get(key)
-
-        if item is None:
-            item = self.get_method(key)
-
-        # If we successfully obtained an item, save it to the cache
-        if item is not None:
-            self.store[key] = item
-
-        return item  # May return None if the line has no task
-
-    def __setitem__(self, key, value):
-        # Never store None in the cache, treat it as deletion
-        if value is None:
-            if key in self:
-                del self[key]
-            return
-
-        # Otherwise store the given value
-        self.store[key] = value
-
-    def values(self):
-        return self.store.values()
-
-    def iteritems(self):
-        return self.store.iteritems()
-
-
-class TaskStore(NoNoneStore):
-
-    def get_method(self, key):
-        return key.tw.tasks.get(uuid=key.value)
-
-
-class VwtaskStore(NoNoneStore):
-
-    def get_method(self, line):
-        return vwtask.VimwikiTask.from_line(self.cache, line)
-
-
-class ViewportStore(NoNoneStore):
-
-    def get_method(self, line):
-        return viewport.ViewPort.from_line(line, self.cache)
-
-
-class LineStore(NoNoneStore):
-
-    def get_method(self, key):
-        cls, line = key
-        return cls.parse_line(line)
+import store
 
 
 class TaskCache(object):
@@ -123,11 +14,16 @@ class TaskCache(object):
     """
 
     def __init__(self):
-        self.task = TaskStore(self)
-        self.vwtask = VwtaskStore(self)
-        self.viewport = ViewportStore(self)
-        self.line = LineStore(self)
-        self.warriors = WarriorStore()
+        # Determine defaults
+        default_rc = vim.vars.get('taskwiki_taskrc_location') or '~/.taskrc'
+        default_data = vim.vars.get('taskwiki_data_location') or '~/.task'
+        extra_warrior_defs = vim.vars.get('taskwiki_extra_warriors', {})
+
+        self.task = store.TaskStore(self)
+        self.vwtask = store.VwtaskStore(self)
+        self.viewport = store.ViewportStore(self)
+        self.line = store.LineStore(self)
+        self.warriors = store.WarriorStore(default_rc, default_data, extra_warrior_defs)
         self.buffer_has_authority = True
 
     @property
---
+++ taskwiki/store.py
@@ -0,0 +1,109 @@
+from tasklib import TaskWarrior
+
+import errors
+import vwtask
+import viewport
+
+
+class WarriorStore(object):
+    """
+    Stores all instances of TaskWarrior objects.
+    """
+
+    def __init__(self, default_rc, default_data, extra_warrior_defs):
+        default_kwargs = dict(
+            data_location=default_data,
+            taskrc_location=default_rc,
+        )
+
+        # Setup the store of TaskWarrior objects
+        self.warriors = {'default': TaskWarrior(**default_kwargs)}
+
+        for key in extra_warrior_defs.keys():
+            current_kwargs = default_kwargs.copy()
+            current_kwargs.update(extra_warrior_defs[key])
+            self.warriors[key] = TaskWarrior(**current_kwargs)
+
+        # Make sure context is not respected in any TaskWarrior
+        for tw in self.warriors.values():
+            tw.overrides.update({'context':''})
+
+    def __getitem__(self, key):
+        try:
+            return self.warriors[key]
+        except KeyError as e:
+            raise errors.TaskWikiException(
+                "Taskwarrior with key '{0}' not available."
+                .format(key))
+
+
+    def __setitem__(self, key, value):
+        self.warriors[key] = value
+
+    def values(self):
+        return self.warriors.values()
+
+    def iteritems(self):
+        return self.warriors.iteritems()
+
+
+class NoNoneStore(object):
+
+    def __init__(self, cache):
+        self.cache = cache
+        self.store = dict()
+
+    def __getitem__(self, key):
+        item = self.store.get(key)
+
+        if item is None:
+            item = self.get_method(key)
+
+        # If we successfully obtained an item, save it to the cache
+        if item is not None:
+            self.store[key] = item
+
+        return item  # May return None if the line has no task
+
+    def __setitem__(self, key, value):
+        # Never store None in the cache, treat it as deletion
+        if value is None:
+            if key in self:
+                del self[key]
+            return
+
+        # Otherwise store the given value
+        self.store[key] = value
+
+    def values(self):
+        return self.store.values()
+
+    def iteritems(self):
+        return self.store.iteritems()
+
+
+class TaskStore(NoNoneStore):
+
+    def get_method(self, key):
+        return key.tw.tasks.get(uuid=key.value)
+
+
+class VwtaskStore(NoNoneStore):
+
+    def get_method(self, line):
+        return vwtask.VimwikiTask.from_line(self.cache, line)
+
+
+class ViewportStore(NoNoneStore):
+
+    def get_method(self, line):
+        return viewport.ViewPort.from_line(line, self.cache)
+
+
+class LineStore(NoNoneStore):
+
+    def get_method(self, key):
+        cls, line = key
+        return cls.parse_line(line)
+
+
