9eea61d6f1c834563df66ac842d9d8eccbb6b335
DancingQuanta <andytheseeker@gmail.com>
2017-07-04 23:37:05 +0100

    Added markdown and restructuredtext syntax to regexp and moved bits around in there

--- taskwiki/regexp.py
+++ taskwiki/regexp.py
@@ -19,6 +19,16 @@ TEXT = r'(?P<text>.+?)'
 COMPLETION_MARK = r'(?P<completed>.)'
 PRIORITY = r'(?P<priority>!{1,3})'
 
+DATETIME_FORMAT = "(%Y-%m-%d %H:%M)"
+DATE_FORMAT = "(%Y-%m-%d)"
+
+ANSI_ESCAPE_SEQ = re.compile(
+    '\x1b'     # literal ESC
+    '\['       # literal [
+    '[;\d]*'   # zero or more digits or semicolons
+    '[A-Za-z]' # a letter
+    )
+
 GENERIC_TASK = re.compile(''.join([
     '^',
     EMPTY_SPACE,
@@ -38,39 +48,98 @@ GENERIC_TASK = re.compile(''.join([
     '$'    # Enforce match on the whole line
 ]))
 
-DATETIME_FORMAT = "(%Y-%m-%d %H:%M)"
-DATE_FORMAT = "(%Y-%m-%d)"
+VIEWPORT = {
+    'default':
+    re.compile(
+        '^'                     # Starts at the begging of the line
+        '[=]+'                  # Heading begging
+        '(?P<name>[^=\|\[\{]*)' # Name of the viewport, all before the | sign
+                                # Cannot include '[', '=', '|, and '{'
+        '\|'                    # Colon
+        '(?P<filter>[^=\|]+?)'  # Filter
+        '('                     # Optional defaults
+        '\|'                    # Colon
+        '(?P<defaults>[^=\|]+?)'# Default attrs
+        ')?'
+        '\s*'                   # Any whitespace
+        '(#(?P<source>[A-Z]))?' # Optional source indicator
+        '\s*'                   # Any whitespace
+        '(\$(?P<sort>[A-Z]))?'  # Optional sort indicator
+        '\s*'                   # Any whitespace
+        '[=]+'                  # Header ending
+    ),
+    'markdown':
+    re.compile(
+        '^'                     # Starts at the begging of the line
+        '[#]+'                  # Heading begging
+        '\s'                    # A whitespace
+        '(?P<name>[^#\|\[\{]*)' # Name of the viewport, all before the | sign
+                                # Cannot include '[', '#', '|, and '{'
+        '\|'                    # Colon
+        '(?P<filter>[^\|]+?)'   # Filter
+        '('                     # Optional defaults
+        '\|'                    # Colon
+        '(?P<defaults>[^\|]+?)' # Default attrs
+        ')?'
+        '\s*'                   # Any whitespace
+        '(#(?P<source>[A-Z]))?' # Optional source indicator
+        '\s*'                   # Any whitespace
+        '(\$(?P<sort>[A-Z]))?'  # Optional sort indicator
+        '\s*'                   # Any whitespace
+        '([#]+)?'               # Optional Header ending
+    ),
+    'restructuredtext':
+    re.compile(
+        '^'                         # Starts at the beginning of the first line
+        '[^\s]'                     # No whitespace
+        '(?P<name>[^\|\[\{]*)'      # Name of the viewport, all before the | sign
+                                    # Cannot include '[', '#', '|, and '{'
+        '\|'                        # Colon
+        '(?P<filter>[^\|]+?)'       # Filter
+        '('                         # Optional defaults
+        '\|'                        # Colon
+        '(?P<defaults>[^\|]+?)'     # Default attrs
+        ')?'
+        '\s*'                       # Any whitespace
+        '(#(?P<source>[A-Z]))?'     # Optional source indicator
+        '\s*'                       # Any whitespace
+        '(\$(?P<sort>[A-Z]))?'      # Optional sort indicator
 
-GENERIC_VIEWPORT = re.compile(
-    '^'                          # Starts at the begging of the line
-    '[=]+'                       # Heading begging
-    '(?P<name>[^=\|\[\{]*)'      # Name of the viewport, all before the | sign
-                                 # Cannot include '[', '=', '|, and '{'
-    '\|'                         # Colon
-    '(?P<filter>[^=\|]+?)'       # Filter
-    '('                          # Optional defaults
-      '\|'                       # Colon
-      '(?P<defaults>[^=\|]+?)'   # Default attrs
-    ')?'
-    '\s*'                        # Any whitespace
-    '(#(?P<source>[A-Z]))?'      # Optional source indicator
-    '\s*'                        # Any whitespace
-    '(\$(?P<sort>[A-Z]))?'       # Optional sort indicator
-    '\s*'                        # Any whitespace
-    '[=]+'                       # Header ending
-    )
+        '[^\s]'                     # No whitespace
+        '\r?\n'                     # Portable newlines
+        r"""([-=~:^"#*._+`'])\1+""" # Any non-alphanumeric characters
+        '$'                         # End of line
+    ).
+}
 
-GENERIC_HEADER = re.compile(
-    '^'        # Starts at the beginning of the line
-    '[=]+'     # With a positive number of =
-    '[^=]+'    # Character other than =
-    '[=]+'     # Positive number of =, closing the header
-    '\s*'      # Allow trailing whitespace
-)
+HEADER = {
+    'default':
+    re.compile(
+        '^'        # Starts at the beginning of the line
+        '[=]+'     # With a positive number of =
+        '[^=]+'    # Character other than =
+        '[=]+'     # Positive number of =, closing the header
+        '\s*'      # Allow trailing whitespace
+    ),
+    'markdown':
+    re.compile(
+        '^'        # Starts at the beginning of the line
+        '[#]+'     # With a positive number of #
+        '\s'       # A whitespace
+        '.'        # Any character
+        '([#]+)?'  # Optional Header ending
+        '$'        # End of line
+    ),
+    'restructuredtext':
+    re.compile(
+        '^'                         # Starts at the beginning of the first line
+        '[^\s]+'                    # No whitespace
+        '.*'                        # Any characters
+        '[^\s]+'                    # No whitespace
+        '\r?\n'                     # Portable newlines
+        r"""([-=~:^"#*._+`'])\1+""" # Any non-alphanumeric characters
+        '$'                         # End of line
+    ).
+}
 
-ANSI_ESCAPE_SEQ = re.compile(
-    '\x1b'     # literal ESC
-    '\['       # literal [
-    '[;\d]*'   # zero or more digits or semicolons
-    '[A-Za-z]' # a letter
-    )
+
