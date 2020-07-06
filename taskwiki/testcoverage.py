184090d365b0407f9786ab3c739004e739770c73
Tomas Janousek <tomi@nomi.cz>
2020-07-06 20:52:59 +0200

    tests: More robust (and simpler) coverage tracking
    Let coverage.py handle generating the filename and saving data at exit.
    This is more robust: it adds a random suffix as well so if one process
    creates two Coverage objects, they don't write to the same file like we
    did. This is another way to fix the inaccurate coverage reporting.
    
    From my experiments it seems that the first Coverage object started (and
    thus the last that saves its data) only covers the loading of modules
    (defs, classes, etc.) whereas the second one, whose data is overwritten
    by the first, contains the actual coverage generated by running the
    tests.

--- taskwiki/testcoverage.py
+++ taskwiki/testcoverage.py
@@ -1,20 +1,3 @@
-import os
-import atexit
 import coverage
 
-class CoverageSaver(object):
-
-    def __init__(self, cov):
-        self.cov = cov
-
-    def __call__(self):
-        self.cov.stop()
-        self.cov.save()
-
-
-coverage_path = '/tmp/taskwiki-coverage/.coverage.{0}'.format(os.getpid())
-cov = coverage.coverage(data_file=coverage_path)
-cov.start()
-
-saver = CoverageSaver(cov)
-atexit.register(saver)
+coverage.Coverage(auto_data=True, data_suffix=True).start()
