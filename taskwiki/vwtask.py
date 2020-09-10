5740aaa5b2815caf2636f65359171c866ac10052
Tomas Janousek <tomi@nomi.cz>
2020-09-10 10:45:19 +0200

    fixup! Make task inherit parent task tag and project

--- taskwiki/vwtask.py
+++ taskwiki/vwtask.py
@@ -165,13 +165,12 @@ class VimwikiTask(object):
         if self.parent:
             self.parent.add_dependencies |= set([self])
 
-        # Make task inherit parent task tag and project
-        if self.parent:
-            self.task['tags'].update(self.parent.task['tags'])
-            self.task['project'] = self.parent.task['project']
-
-        # For new tasks, apply defaults from above viewport
+        # For new tasks, apply defaults from parent and above viewport
         if not self.uuid:
+            if self.parent:
+                self['tags'] = self.parent['tags']
+                self['project'] = self.parent['project']
+
             self.apply_defaults()
 
             # If -- is in description, assume it's separator for metadata
