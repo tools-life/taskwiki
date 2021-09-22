tools-life/taskwiki

commit d98d5b18acd12fd2a7046b76a1f5d6038403aea9

ci: Fix coveralls submission
See [0] for reference.

[0]: https://github.com/lemurheavy/coveralls-public/issues/1435

Makefile
@@ -10,7 +10,7 @@ pytest:
 
 cover-pytest: PYTEST_FLAGS += --cov=taskwiki
 cover-pytest: pytest
-	if [ "$$GITHUB_ACTIONS" ]; then coveralls || :; fi
+	if [ "$$GITHUB_ACTIONS" ]; then coveralls --service=github || :; fi
 
 xvfb-%:
 	xvfb-run --server-args=-noreset $(MAKE) $*

.github/workflows/tests.yaml
@@ -70,7 +70,7 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: Finished
-        uses: coverallsapp/github-action@v1.1.1
+        uses: coverallsapp/github-action@1.1.3
         with:
           github-token: ${{ secrets.GITHUB_TOKEN }}
           parallel-finished: true

commit c4392c36c01cfffcb06f460d722990b98bc53bdb

ci: Revert unnecessary part of the coveralls "fix"
We already set this via the COVERALLS_SERVICE_NAME environment variable
in the GitHub Actions workflow, as it's really specific to GHA.

And as it turns out, the fix wasn't really necessary anyway as the
Coveralls.io infrastructure was just temporarily down.

Fixes: d98d5b18acd1 ("ci: Fix coveralls submission")

Makefile
@@ -10,7 +10,7 @@ pytest:
 
 cover-pytest: PYTEST_FLAGS += --cov=taskwiki
 cover-pytest: pytest
-	if [ "$$GITHUB_ACTIONS" ]; then coveralls --service=github || :; fi
+	if [ "$$GITHUB_ACTIONS" ]; then coveralls || :; fi
 
 xvfb-%:
 	xvfb-run --server-args=-noreset $(MAKE) $*